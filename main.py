fb8121f0fcd7137532dc0b82fe047cac1dabd338
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-05-10 16:53:22 +0300

    Dynamic number of processes input

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from Process import get_processes
-from sjf import schedule_sjf
+from scheduling import schedule
 from utils import calculate_metrics, display_results
 
-processes = get_processes(3)
-scheduling_order = schedule_sjf(processes)
+num_of_processes = input("How many processes? ")
+try:
+    num_of_processes = int(num_of_processes)
+except Exception as e:
+    raise e
+
+processes = get_processes(num_of_processes)
+scheduling_order = schedule(processes)
 metrics = calculate_metrics(scheduling_order)
 
 display_results(scheduling_order, metrics)
-# for process in scheduling_order:
-#     print(process)
-# print(f"Avarage waiting time: {metrics['waiting']}\n"
-#       f"Avarage turnaround time: {metrics['turnaround']}")
